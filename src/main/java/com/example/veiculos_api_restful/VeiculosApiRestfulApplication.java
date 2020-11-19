6c0afa1bfaa6a67ca13821b4d57801d6c9e606ea
RaquelKO <kuntzraquel@gmail.com>
2020-11-18 22:28:06 -0300

    first commit

---
+++ src/main/java/com/example/veiculos_api_restful/VeiculosApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.veiculos_api_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/veiculos_api_restful/VeiculosApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veiculos_api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
